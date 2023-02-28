ec27a4b1c04e0776672e01cb4c5f944714ad9302
matan.parker <matan.parker@salesforce.com>
2023-02-28 19:21:57 +0200

    update

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import webbrowser
 MODEL_ENGINE = "text-davinci-003"
 MAX_TOKENS = 2048
 openai.api_key = os.getenv('OPEN_AI_API_KEY')
+n = 5
+
+prompt = "create an html web page for a site about breeds of cats with bootstrap 5. the site should have " \
+         "a bootstarp nav bar including CatBreeds brand name, about, contact and blog. the first section should represent the site" \
+         "idea and visionand  the passion of the owner about cats. the second section should contain" \
+         "a bootstrap carousel with 3 breads of cats each with its photo from picsum.photos and description, the carousel should have " \
+         "navigation buttons. " \
+         "the last section should contain a footer with all the contact information." \
+         "style the site with css to make it approachable and convenient."
 
 
 def generate_plain_response(prompt):
@@ -35,25 +44,16 @@ def generate_response_with_context(prompt, context):
 
 
 def generate_html():
-    prompt = "create an html web page for a site about breeds of cats with bootstrap 5. the site should have " \
-             "a bootstarp nav bar with: CatBreeds brand name about, contact and blog. the first section should represent the site" \
-             "idea and vision, the passion of the owner about cats in 300 words. the second section should contain" \
-             "a bootstrap carousel with 3 breads of cats each with its photo from picsum.photos and description the carousel should have " \
-             "navigation buttons. " \
-             "the last section should contain a footer with all the contact information." \
-             "style the site with css to make it approachable and convenient. do not include integrity checks for " \
-             "scripts you add the web site" \
-             "check that you have </html> closing tag at the end of the site, if not keep generating the content."
-
     html_content = generate_plain_response(prompt)
     for i in range(0, 10):
         answer = html_feedback_loop(html_content,
                                     "if the html in the context is valid return Yes else complete the missing parts in the page")
-        if "Yes" in answer:
+        first_n_words_of_answer = answer.split()[:n]
+        print(first_n_words_of_answer)
+        if "Yes" in first_n_words_of_answer:
             break
         else:
             html_content = answer
-
     return html_content
 
 
